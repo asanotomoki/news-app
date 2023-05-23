18938a634d4bc5dc52159d670b7b9a9b9f678cfd
tasano <mirai20190820@gmail.com>
2023-05-24 04:59:07 +0900

    pre test

--- src/components/newsCard/index.tsx
+++ src/components/newsCard/index.tsx
@@ -16,7 +16,7 @@ export const NewsCard: React.FC<Props> = (props) => {
     console.log('click')
   }
   return (
-    <article className="flex justify-between items-center w-full  bg-white p-6 min-h-max">
+    <article className="flex justify-between items-center w-full  bg-white p-6 min-h-max shadow-lg">
       <div className="flex flex-col justify-between gap-3 w-2/3">
         <Link href={url}>
           <h3 className="text-2xl font-bold text-gray-950 hover:underline hover:text-gray-700">
@@ -28,7 +28,7 @@ export const NewsCard: React.FC<Props> = (props) => {
           <PrimaryButton onClick={onClick}>保存</PrimaryButton>
         </div>
       </div>
-      <div className="w-1/3 ml-8 max-h-48 overflow-hidden flex items-center">
+      <div className="w-1/3 ml-8 max-h-48 overflow-hidden flex items-center justify-center">
         <img src={image} alt={title} />
       </div>
     </article>
