cb530201a6221a4fb69097339fd7c8e73e12d6c2
tasano <mirai20190820@gmail.com>
2023-06-01 15:55:56 +0900

    comment out graphql

--- src/graphql/context.ts
+++ src/graphql/context.ts
@@ -1,23 +1,23 @@
-import type { User } from '@prisma/client'
-import type { IncomingMessage } from 'http'
-import { getSession } from 'next-auth/react'
+//import type { User } from '@prisma/client'
+//import type { IncomingMessage } from 'http'
+//import { getSession } from 'next-auth/react'
 
-import { prisma } from '@/lib/prisma'
+//import { prisma } from '@/lib/prisma'
 
-export type Context = {
-  prisma: typeof prisma
-  currentUser: User | null
-}
+//export type Context = {
+//  prisma: typeof prisma
+//  currentUser: User | null
+//}
 
-export const createContext = async ({
-  req,
-}: {
-  req: IncomingMessage
-}): Promise<Context> => {
-  const session = await getSession({ req })
-  const email = session?.user?.email
-  const currentUser = email
-    ? await prisma.user.findUnique({ where: { email } })
-    : null
-  return { prisma, currentUser }
-}
+//export const createContext = async ({
+//  req,
+//}: {
+//  req: IncomingMessage
+//}): Promise<Context> => {
+//  const session = await getSession({ req })
+//  const email = session?.user?.email
+//  const currentUser = email
+//    ? await prisma.user.findUnique({ where: { email } })
+//    : null
+//  return { prisma, currentUser }
+//}
